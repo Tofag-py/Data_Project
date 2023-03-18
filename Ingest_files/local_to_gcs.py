f045c37a0a6be365fa6a914e27bb1a2d322e2beb
Olesev <Fagbuyit@yahoo.com>
2023-03-18 03:05:23 +0100

    Updated local to gcs file

---
+++ Ingest_files/local_to_gcs.py
@@ -0,0 +1,72 @@
+from web_to_local import append_csv_to_mega_file
+from prefect import flow, task
+import pandas as pd
+from prefect_gcp.cloud_storage import GcsBucket
+
+
+# Convert the mega_file to df
+@task(log_prints=True)
+def read_csv(dataset_file: str) -> pd.DataFrame:
+    df = pd.read_csv(dataset_file)
+    return df
+
+@task(log_prints=True)
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+     """Fix dtype issues"""
+     df = df.loc[df['FIPS'] != 'FIPS']
+     df = df.loc[df['Admin2'] != 'Admin2']
+     df = df.loc[df['Province_State'] != 'Province_State']
+     df = df.loc[df['Country_Region'] != 'Country_Region']
+     df = df.loc[df['Last_Update'] != 'Last_Update']
+     df = df.loc[df['Lat'] != 'Lat']
+     df = df.loc[df['Long_'] != 'Long_']
+     df = df.loc[df['Confirmed'] != 'Confirmed']
+     df = df.loc[df['Deaths'] != 'Deaths']
+     df = df.loc[df['Recovered'] != 'Recovered']
+     df = df.loc[df['Active'] != 'Active']
+     df = df.loc[df['Combined_Key'] != 'Combined_Key']
+     df = df.loc[df['Incident_Rate'] != 'Incident_Rate']
+     df = df.loc[df['Case_Fatality_Ratio'] != 'Case_Fatality_Ratio']
+     df['Last_Update'] = pd.to_datetime(df['Last_Update'])
+     df = df.astype({
+         'FIPS': 'object',
+         'Admin2': 'object',
+         'Province_State': 'category',
+         'Country_Region': 'object',
+         'Lat': 'float',
+         'Long_': 'float',
+         'Confirmed': 'float',
+         'Deaths': 'float',
+         'Recovered': 'float',
+         'Active': 'float',
+         'Combined_Key': 'object'
+          })
+     return df
+
+#Convert file to parquet
+@task(log_prints=True)
+def csv_to_parquet(df: pd.DataFrame) -> pd.DataFrame:
+    df.to_parquet('Covid_file.parquet')
+    return df
+     
+#Write file to gcs
+@task()
+def write_gcs(df: pd.DataFrame, mega_filepath) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("project-bucket")
+    gcs_block.upload_from_path(from_path=mega_filepath, to_path=mega_filepath)
+    return
+
+#Run the tasks
+@flow(log_prints=True)
+def run_tasks():
+    
+    mega_file = append_csv_to_mega_file.map(csv_data, dates, data_dir)
+    df = read_csv(mega_file())
+    df_cleaned = clean_data(df)
+    csv_to_parquet(df_cleaned)
+    write_gcs(csv_to_parquet, append_csv_to_mega_file)
+
+# Run the flow
+if __name__ == "__main__":
+    run_tasks()
---
+++ Ingest_files/web_to_local.py
@@ -0,0 +1,75 @@
+import os
+import requests
+from prefect import task, flow
+import datetime
+import pandas as pd
+from prefect_gcp.cloud_storage import GcsBucket
+
+# Create the data directory if it does not exist
+@task(log_prints=True)
+def make_path(data_dir):
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+# Download csv from web
+@task(log_prints=True)
+def get_csv(date, base_url):
+    """Downloads the CSV file for the given date."""
+    url = base_url + date.strftime("%m-%d-%Y") + ".csv"
+    response = requests.get(url)
+    try:
+        response.raise_for_status()
+    except requests.exceptions.HTTPError as e:
+        print(f"HTTP error occurred while downloading data for {date}: {e}")
+        return None
+    return response.content
+
+@task(log_prints=True)
+def save_csv(data, date, data_dir):
+    """Saves the CSV data to a file with the given date."""
+    filename = os.path.join(data_dir, date.strftime("%m-%d-%Y") + ".csv")
+    with open(filename, "wb") as f:
+        f.write(data)
+
+# Append the csv files to a mega file
+@task(log_prints=True)
+def append_csv_to_mega_file(csv_data, date, data_dir) -> str:
+    """Appends the CSV data to a mega file and stores it locally."""
+    mega_filename = "mega_csv_file.csv"
+    mega_filepath = os.path.join(data_dir, mega_filename)
+    csv_filename = date.strftime("%m-%d-%Y") + ".csv"
+    csv_filepath = os.path.join(data_dir, csv_filename)
+
+    # Append the CSV data to the mega file
+    with open(mega_filepath, "a") as mega_file:
+        mega_file.write(csv_data.decode("utf-8"))
+
+    # Delete the CSV file after it has been appended to the mega file
+    os.remove(csv_filepath)
+    return mega_filepath
+
+#Run the tasks
+@flow(log_prints=True)
+def run_tasks():
+    # Define the dates for which to download the CSV files
+    start_date = datetime.date(2020, 1, 22)
+    end_date = datetime.date.today()
+    dates = [start_date + datetime.timedelta(days=x) for x in range((end_date - start_date).days + 1)]
+
+    # Define the base URL for the CSV files
+    base_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/"
+
+    # Define the directory to store the downloaded CSV files
+    data_dir = "Ingest_files/covid_data"
+
+    # Define the Prefect flow
+    make_path(data_dir)
+    csv_data = get_csv.map(dates, base_url)
+    csv_files = save_csv.map(csv_data, dates, data_dir)
+    mega_file = append_csv_to_mega_file.map(csv_data, dates, data_dir)
+       
+
+# Run the flow
+if __name__ == "__main__":
+    run_tasks()
+
